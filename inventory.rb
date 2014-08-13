76b3bc3a5c7e6ca595d028ca24bf7cc16f48d0ff
Chris Rennie <ebookren@yahoo.com>
2014-08-13 08:16:54 -0400

    Remove item decrease itme count.

--- inventory.rb
+++ inventory.rb
@@ -21,18 +21,31 @@ puts @items.inspect
   puts " (A)dd item\n (R)emove item\n (L)ist items\n (I)ncrease item\n (D)ecrease items\n (Q)uit "
   select = [(print '?: '), gets.rstrip][1]
  
-if (select.upcase=="Q")
-   exit
 
-  elsif (select.upcase=="A")
+  if (select.upcase=="A") then
   puts "Add Item\nItem name"
   name=[(print 'Name? : '), gets.rstrip][1]
   puts "Initial Count"
   count= [(print 'Count? : '), gets.rstrip][1]
   @items.push(Item.new(name,count.to_i))
+
+  end
   
-  prompt()
-elsif (select.upcase=="I")
+  if (select.upcase=="R") then
+  puts "Remove Item\nItem name"
+  name=[(print 'Name? : '), gets.rstrip][1]
+  match_index = @items.index(@items.find { |l| l.name == name })
+  if (match_index)  
+    @items.delete_at(match_index)
+  end
+
+  end  
+
+if (select.upcase=="Q") then
+   exit
+  end
+
+if (select.upcase=="I") then
   puts "Item Name"
   name=[(print 'Name? : '), gets.rstrip][1].downcase
   match_index = @items.index(@items.find { |l| l.name == name })
@@ -44,8 +57,25 @@ puts "Item #{name} Count #{@items[match_index].count}"
   else 
    puts "That item is not counted"
 	end
- 
-  end  
+  
+end
+
+if (select.upcase=="D") then
+  puts "Item Name"
+  name=[(print 'Name? : '), gets.rstrip][1].downcase
+  match_index = @items.index(@items.find { |l| l.name == name })
+  if (match_index)  
+   
+puts "Item #{name} Count #{@items[match_index].count}"
+  amount=[(print 'Decrease amount? : '), gets.rstrip][1]
+  @items[match_index].sub(amount.to_i)
+  else 
+   puts "That item is not counted"
+	end
+  
+end
+
+
   prompt()
 end
 
